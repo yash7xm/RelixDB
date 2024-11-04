b77349f23c57a2498a6a496dab409120d1d0aecd
yash7xm <yash.7xm@gmail.com>
2024-11-04 15:12:17 +0530

    insert interface added

--- BTree.go
+++ BTree.go
@@ -212,7 +212,7 @@ func treeInsert(tree *BTree, node BNode, key []byte, val []byte) BNode {
 		}
 	case BNODE_NODE:
 		// internal node insert it to a kid node.
-		// nodeInsert(tree, new, node, idx, key, val)
+		nodeInsert(tree, new, node, idx, key, val)
 	default:
 		panic("bad node!")
 	}
@@ -386,4 +386,41 @@ func (tree *BTree) Delete(key []byte) bool {
 	}
 
 	return true
+}
+
+// interface for inserting
+func (tree *BTree) Insert(key []byte, val []byte) {
+	assert(len(key) != 0, "key is not provided")
+	assert(len(key) <= BTREE_MAX_KEY_SIZE, "key provided exccedes the max size")
+	assert(len(val) <= BTREE_MAX_VAL_SIZE, "val provided exccedes the max size")
+
+	if tree.root == 0 {
+		// create the first node
+		root := BNode{data: make([]byte, BTREE_PAGE_SIZE)}
+		root.setHeader(BNODE_LEAF, 2)
+		// a dummy key, this makes the tree cover the whole key space.
+		// thus a lookup can always find a containg node..
+		nodeAppendKV(root, 0, 0, nil, nil)
+		nodeAppendKV(root, 1, 0, key, val)
+		tree.root = tree.new(root)
+		return
+	}
+
+	node := tree.get(tree.root)
+	tree.del(tree.root)
+
+	node = treeInsert(tree, node, key, val)
+	nsplit, splitted := nodeSplit3(node)
+	if nsplit > 1 {
+		// the root was split, add a new level.
+		root := BNode{data: make([]byte, BTREE_PAGE_SIZE)}
+		root.setHeader(BNODE_NODE, nsplit)
+		for i, knode := range splitted[:nsplit] {
+			ptr, key := tree.new(knode), knode.getKey(0)
+			nodeAppendKV(root, uint16(i), ptr, key, nil)
+		}
+		tree.root = tree.new(root)
+	} else {
+		tree.root = tree.new(splitted[0])
+	}
 }
