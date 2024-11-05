dd0238363fef0ee366213374c09a30cf0e769c10
yash7xm <yash.7xm@gmail.com>
2024-11-05 12:34:56 +0530

    mmap init added

---
+++ KV/KV.go
@@ -0,0 +1,38 @@
+package kv
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"syscall"
+
+	BTree "github.com/yash7xm/RelixDB/BTree"
+)
+
+func mmapInit(fp *os.File) (int, []byte, error) {
+	fi, err := fp.Stat()
+	if err != nil {
+		return 0, nil, fmt.Errorf("stat: %w", err)
+	}
+
+	if fi.Size()%BTree.BTREE_PAGE_SIZE != 0 {
+		return 0, nil, errors.New("file size is not a multiple of page size")
+	}
+
+	mmapSize := 64 << 20
+	BTree.Assert(mmapSize%BTree.BTREE_PAGE_SIZE == 0, "mmap size is not a multiple of page size.")
+	for mmapSize < int(fi.Size()) {
+		mmapSize *= 2
+	}
+	// mmap size can be larger than the file
+
+	chunk, err := syscall.Mmap(
+		int(fp.Fd()), 0, mmapSize,
+		syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED,
+	)
+	if err != nil {
+		return 0, nil, fmt.Errorf("mmap: %w", err)
+	}
+
+	return int(fi.Size()), chunk, nil
+}
