116732685da05c5d10483dbe74730187f8020938
yash7xm <yash.7xm@gmail.com>
2024-11-06 11:57:28 +0530

    cells and records added

---
+++ BTree/RelationalDB.go
@@ -0,0 +1,25 @@
+package BTree
+
+const (
+	TYPE_ERROR = 0
+	TYPE_BYTES = 1
+	TYPE_INT64 = 2
+)
+
+// table cell
+type Value struct {
+	Type uint32
+	I64  int64
+	Str  []byte
+}
+
+// table row
+type Record struct {
+	Cols []string
+	Vals []Value
+}
+
+func (rec *Record) AddStr(key string, val []byte) *Record
+func (rec *Record) AddInt64(key string, val int64) *Record
+func (rec *Record) Get(key string) *Value
+
