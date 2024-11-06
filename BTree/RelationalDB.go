e55b4a379ccc59c819ff3ad102a87df5779cb449
yash7xm <yash.7xm@gmail.com>
2024-11-06 12:13:07 +0530

    table def and db added

--- BTree/RelationalDB.go
+++ BTree/RelationalDB.go
@@ -23,3 +23,38 @@ func (rec *Record) AddStr(key string, val []byte) *Record
 func (rec *Record) AddInt64(key string, val int64) *Record
 func (rec *Record) Get(key string) *Value
 
+type DB struct {
+	Path string
+	// internals
+	kv     KV
+	tables map[string]*TableDef // cached table defination
+}
+
+// table defination
+type TableDef struct {
+	// user defined
+	Name  string
+	Types []uint32 // column types
+	Cols  []string // column names
+	PKeys int      // the first `PKeys` columns are the primary key
+	// auto-assigned B-tree key prefixes for different tables
+	Prefix uint32
+}
+
+// internal table : metadata
+var TDEF_META = &TableDef{
+	Prefix: 1,
+	Name:   "@meta",
+	Types:  []uint32{TYPE_BYTES, TYPE_BYTES},
+	Cols:   []string{"key", "val"},
+	PKeys:  1,
+}
+
+// internal table: table schemas
+var TDEF_TABLE = &TableDef{
+	Prefix: 2,
+	Name:   "@table",
+	Types:  []uint32{TYPE_BYTES, TYPE_BYTES},
+	Cols:   []string{"name", "def"},
+	PKeys:  1,
+}
