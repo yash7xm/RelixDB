c35b876a1d84a40ef78c6e4ba5866822df75f898
yash7xm <yash.7xm@gmail.com>
2024-11-06 17:52:25 +0530

    point query added

--- BTree/RelationalDB.go
+++ BTree/RelationalDB.go
@@ -1,5 +1,11 @@
 package BTree
 
+import (
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+)
+
 const (
 	TYPE_ERROR = 0
 	TYPE_BYTES = 1
@@ -102,20 +108,91 @@ var TDEF_TABLE = &TableDef{
 }
 
 // get a single row by primary key
-// func dbGet(db *DB, tdef *TableDef, rec *Record) (bool, error) {
-// 	values, err := checkRecord(tdef, *rec, tdef.PKeys)
-// 	if err != nil {
-// 		return false, err
-// 	}
+func dbGet(db *DB, tdef *TableDef, rec *Record) (bool, error) {
+	values, err := checkRecord(tdef, *rec, tdef.PKeys)
+	if err != nil {
+		return false, err
+	}
 
-// 	fmt.Println(values)
+	key := encodeKey(nil, tdef.Prefix, values[:tdef.PKeys])
+	val, ok := db.kv.Get(key)
+	if !ok {
+		return false, nil
+	}
 
-// 	return true, nil
-// }
+	for i := tdef.PKeys; i < len(tdef.Cols); i++ {
+		values[i].Type = tdef.Types[i]
+	}
+
+	decodeValues(val, values[tdef.PKeys:])
+
+	rec.Cols = append(rec.Cols, tdef.Cols[tdef.PKeys:]...)
+	rec.Vals = append(rec.Vals, values[tdef.PKeys:]...)
+
+	return true, nil
+}
 
 // reorder a record and check for missing columns.
 // n == tdef.PKeys: record is excatly a primary key
 // n == len(tdef.Cols): record containse all columns.
-// func checkRecord(tdef *TableDef, rec Record, n int) ([]Value, error) {
+func checkRecord(tdef *TableDef, rec Record, n int) ([]Value, error) {
+	if n == tdef.PKeys {
+		return rec.Vals, nil
+	}
 
-// }
+	if n == len(tdef.Cols) {
+		return rec.Vals, nil
+	}
+
+	return rec.Vals, fmt.Errorf("record is not valid")
+}
+
+func encodeValues(out []byte, vals []Value) []byte
+func decodeValues(in []byte, out []Value)
+
+// for primary keys
+func encodeKey(out []byte, prefix uint32, vals []Value) []byte {
+	var buf [4]byte
+	binary.BigEndian.PutUint32(buf[:], prefix)
+	out = append(out, buf[:]...)
+	out = encodeValues(out, vals)
+	return out
+}
+
+// get a single row by the primary key
+func (db *DB) Get(table string, rec *Record) (bool, error) {
+	tdef := getTableDef(db, table)
+	if tdef == nil {
+		return false, fmt.Errorf("table not found: %s", table)
+	}
+	return dbGet(db, tdef, rec)
+}
+
+// get the table defination by name
+func getTableDef(db *DB, name string) *TableDef {
+	tdef, ok := db.tables[name]
+	if !ok {
+		if db.tables == nil {
+			db.tables = map[string]*TableDef{}
+		}
+		tdef = getTableDefDB(db, name)
+		if tdef != nil {
+			db.tables[name] = tdef
+		}
+	}
+	return tdef
+}
+
+func getTableDefDB(db *DB, name string) *TableDef {
+	rec := (&Record{}).AddStr("name", []byte(name))
+	ok, err := dbGet(db, TDEF_TABLE, rec)
+	Assert(err == nil, "unable to get def")
+	if !ok {
+		return nil
+	}
+
+	tdef := &TableDef{}
+	err = json.Unmarshal(rec.Get("def").Str, tdef)
+	Assert(err == nil, "unable to get def")
+	return tdef
+}
