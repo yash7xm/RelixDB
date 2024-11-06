32d60832cee518d03c7026906e3c92209ef1ce79
yash7xm <yash.7xm@gmail.com>
2024-11-06 18:01:12 +0530

    table update added

--- BTree/RelationalDB.go
+++ BTree/RelationalDB.go
@@ -196,3 +196,55 @@ func getTableDefDB(db *DB, name string) *TableDef {
 	Assert(err == nil, "unable to get def")
 	return tdef
 }
+
+// modes of the updates
+const (
+	MODE_UPSERT      = 0 // insert or replace
+	MODE_UPDATE_ONLY = 1 // update existing keys
+	MODE_INSERT_ONLY = 2 // only add new keys
+)
+
+type InsertReq struct {
+	tree *BTree
+	// out
+	Added bool // added a new key
+	// in
+	Key  []byte
+	Val  []byte
+	Mode int
+}
+
+func (tree *BTree) InsertEx(req *InsertReq)
+func (db *KV) Update(key []byte, val []byte, mode int) (bool, error)
+
+// add a row to the table
+func dbUpdate(db *DB, tdef *TableDef, rec Record, mode int) (bool, error) {
+	values, err := checkRecord(tdef, rec, len(tdef.Cols))
+	if err != nil {
+		return false, err
+	}
+	key := encodeKey(nil, tdef.Prefix, values[:tdef.PKeys])
+	val := encodeValues(nil, values[tdef.PKeys:])
+	return db.kv.Update(key, val, mode)
+}
+
+// add a record
+func (db *DB) Set(table string, rec Record, mode int) (bool, error) {
+	tdef := getTableDef(db, table)
+	if tdef == nil {
+		return false, fmt.Errorf("table not found: %s", table)
+	}
+	return dbUpdate(db, tdef, rec, mode)
+}
+
+func (db *DB) Insert(table string, rec Record) (bool, error) {
+	return db.Set(table, rec, MODE_INSERT_ONLY)
+}
+
+func (db *DB) Update(table string, rec Record) (bool, error) {
+	return db.Set(table, rec, MODE_UPDATE_ONLY)
+}
+
+func (db *DB) Upsert(table string, rec Record) (bool, error) {
+	return db.Set(table, rec, MODE_UPSERT)
+}
