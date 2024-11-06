2e9c92151b931be68eb3b6ef0518c8c35c6c6b28
yash7xm <yash.7xm@gmail.com>
2024-11-06 14:24:34 +0530

    Record helper fns added

--- BTree/RelationalDB.go
+++ BTree/RelationalDB.go
@@ -19,9 +19,51 @@ type Record struct {
 	Vals []Value
 }
 
-func (rec *Record) AddStr(key string, val []byte) *Record
-func (rec *Record) AddInt64(key string, val int64) *Record
-func (rec *Record) Get(key string) *Value
+func (rec *Record) AddStr(key string, val []byte) *Record {
+	Val := Value{
+		Type: 1,
+		Str:  val,
+	}
+	new := &Record{}
+	for i, colName := range rec.Cols {
+		new.Cols[i] = colName
+		if colName != key {
+			new.Vals[i] = rec.Vals[i]
+		} else {
+			new.Vals[i] = Val
+		}
+	}
+	return new
+}
+
+func (rec *Record) AddInt64(key string, val int64) *Record {
+	Val := Value{
+		Type: 2,
+		I64:  val,
+	}
+	new := &Record{}
+	for i, colName := range rec.Cols {
+		new.Cols[i] = colName
+		if colName != key {
+			new.Vals[i] = rec.Vals[i]
+		} else {
+			new.Vals[i] = Val
+		}
+	}
+	return new
+}
+
+func (rec *Record) Get(key string) *Value {
+	idx := 0
+	for i, colName := range rec.Cols {
+		if colName == key {
+			idx = i
+			break
+		}
+	}
+
+	return &rec.Vals[idx]
+}
 
 type DB struct {
 	Path string
@@ -58,3 +100,22 @@ var TDEF_TABLE = &TableDef{
 	Cols:   []string{"name", "def"},
 	PKeys:  1,
 }
+
+// get a single row by primary key
+// func dbGet(db *DB, tdef *TableDef, rec *Record) (bool, error) {
+// 	values, err := checkRecord(tdef, *rec, tdef.PKeys)
+// 	if err != nil {
+// 		return false, err
+// 	}
+
+// 	fmt.Println(values)
+
+// 	return true, nil
+// }
+
+// reorder a record and check for missing columns.
+// n == tdef.PKeys: record is excatly a primary key
+// n == len(tdef.Cols): record containse all columns.
+// func checkRecord(tdef *TableDef, rec Record, n int) ([]Value, error) {
+
+// }
