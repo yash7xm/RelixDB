f16151d55a6f6113c30b5c25f2f4c8777940e25c
yash7xm <yash.7xm@gmail.com>
2024-11-07 16:46:37 +0530

    rel table add

---
+++ Relational/table.go
@@ -0,0 +1,109 @@
+package relational
+
+import (
+	"fmt"
+
+	BTree "github.com/yash7xm/RelixDB/BTree"
+)
+
+type DB struct {
+	Path string
+	// internals
+	kv     BTree.KV
+	tables map[string]*TableDef // cached table defination
+}
+
+// table defination
+type TableDef struct {
+	// user defined
+	Name  string
+	Types []uint32 // column types
+	Cols  []string // column names
+	PKeys int      // the first `PKeys` columns are the primary key
+	// auto-assigned B-tree key prefixes for different tables
+	Prefix uint32
+}
+
+// internal table : metadata
+var TDEF_META = &TableDef{
+	Name:  "@meta",
+	Types: []uint32{TYPE_BYTES, TYPE_BYTES},
+	Cols:  []string{"key", "val"},
+	PKeys: 1,
+}
+
+// internal table: table schemas
+var TDEF_TABLE = &TableDef{
+	Name:  "@table",
+	Types: []uint32{TYPE_BYTES, TYPE_BYTES},
+	Cols:  []string{"name", "def"},
+	PKeys: 1,
+}
+
+// reorder a record and check for missing columns.
+// n == tdef.PKeys: record is excatly a primary key
+// n == len(tdef.Cols): record containse all columns.
+func checkRecord(tdef *TableDef, rec Record, n int) ([]Value, error) {
+	if len(rec.Cols) < n {
+		return nil, fmt.Errorf("missing columns in the record: expected at least %d, got %d", n, len(rec.Cols))
+	}
+
+	values := make([]Value, len(tdef.Cols))
+
+	// Check that all necessary columns are present in the record
+	colMap := map[string]Value{}
+	for i, col := range rec.Cols {
+		colMap[col] = rec.Vals[i]
+	}
+
+	// Rearrange columns according to table definition
+	for i, col := range tdef.Cols {
+		val, ok := colMap[col]
+		if !ok && i < tdef.PKeys {
+			// Primary key column is missing, return error
+			return nil, fmt.Errorf("missing primary key column: %s", col)
+		}
+
+		values[i] = val
+	}
+
+	return values, nil
+}
+
+// get a single row by the primary key
+func (db *DB) Get(table string, rec *Record) (bool, error) {
+	tdef := getTableDef(db, table)
+	if tdef == nil {
+		return false, fmt.Errorf("table not found: %s", table)
+	}
+	return dbGet(db, tdef, rec)
+}
+
+// get the table defination by name
+func getTableDef(db *DB, name string) *TableDef {
+	tdef, ok := db.tables[name]
+	if !ok {
+		if db.tables == nil {
+			db.tables = map[string]*TableDef{}
+		}
+		tdef = getTableDefDB(db, name)
+		if tdef != nil {
+			db.tables[name] = tdef
+		}
+	}
+	return tdef
+}
+
+func getTableDefDB(db *DB, name string) *TableDef {
+	rec := (&Record{}).AddStr("name", []byte(name))
+	ok, err := dbGet(db, TDEF_TABLE, rec)
+	BTree.Assert(err == nil, "unable to get def")
+	if !ok {
+		return nil
+	}
+
+	tdef := &TableDef{}
+	err = json.Unmarshal(rec.Get("def").Str, tdef)
+	BTree.Assert(err == nil, "unable to get def")
+	return tdef
+}
