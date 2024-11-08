6a1947f5b6e992c42cf7bfb9bff63f6a9ddcfc5f
yash7xm <yash.7xm@gmail.com>
2024-11-08 19:06:58 +0530

    scanner added

--- Relational/scanner.go
+++ Relational/scanner.go
@@ -1,2 +1,197 @@
 package relational
 
+import (
+	"bytes"
+	"fmt"
+
+	BTree "github.com/yash7xm/RelixDB/BTree"
+)
+
+// find the closest position that is less or equal to the input key
+func (tree *BTree.BTree) SeekLE(key []byte) *BIter {
+	iter := &BIter{tree: tree}
+	for ptr := tree.root; ptr != 0; {
+		node := tree.get(ptr)
+		idx := nodeLookupLE(node, key)
+		iter.path = append(iter.path, node)
+		iter.pos = append(iter.pos, idx)
+		if node.btype() == BNODE_NODE {
+			ptr = node.getPtr(idx)
+		} else {
+			ptr = 0
+		}
+	}
+	return iter
+}
+
+const (
+	CMP_GE = +3 // >=
+	CMP_GT = +2 // >
+	CMP_LT = -2 // <
+	CMP_LE = -3 // <=
+)
+
+// find the closest position to a key with respect to `cmp` relation
+func (tree *BTree) Seek(key []byte, cmp int) *BIter {
+	iter := tree.SeekLE(key)
+	if cmp != CMP_LE && iter.Valid() {
+		curr, _ := iter.Deref()
+		if !cmpOK(curr, cmp, key) {
+			// off by one
+			if cmp > 0 {
+				iter.Next()
+			} else {
+				iter.Prev()
+			}
+		}
+	}
+
+	return iter
+}
+
+// key cmp ref
+func cmpOK(key []byte, cmp int, ref []byte) bool {
+	r := bytes.Compare(key, ref)
+	switch cmp {
+	case CMP_GE:
+		return r >= 0
+	case CMP_GT:
+		return r > 0
+	case CMP_LT:
+		return r < 0
+	case CMP_LE:
+		return r <= 0
+	default:
+		panic("what?")
+	}
+}
+
+// the iterator for range queries
+type Scanner struct {
+	// the range, from Key1 to Key2
+	Cmp1 int // CMP_??
+	Cmp2 int
+	Key1 Record
+	Key2 Record
+	// internal
+	db      *DB
+	tdef    *TableDef
+	indexNo int    // -1: use the primary key; >= 0: use an index
+	iter    *BIter // the underlying B-tree iterator
+	keyEnd  []byte // the encoded Key2
+}
+
+// fetch the current row
+func (sc *Scanner) Deref(rec *Record) {
+	BTree.Assert(sc.Valid(), "scanner is not valid")
+
+	tdef := sc.tdef
+	rec.Cols = tdef.Cols
+	rec.Vals = rec.Vals[:0]
+	key, val := sc.iter.Deref()
+
+	if sc.indexNo < 0 {
+		// primary key decode the KV pair
+	} else {
+		// secondary index
+		// the "value" part of the KV store is not used by inde
+		BTree.Assert(len(val) == 0, "value is present to defre index")
+
+		// decode the primary key first
+		index := tdef.Indexes[sc.indexNo]
+		ival := make([]Value, len(index))
+		for i, c := range index {
+			ival[i].Type = tdef.Types[colIndex(tdef, c)]
+		}
+		decodeValues(key[4:], ival)
+		icol := Record{index, ival}
+		// fetch the row by the primary key
+		rec.Cols = tdef.Cols[:tdef.PKeys]
+		for _, c := range rec.Cols {
+			rec.Vals = append(rec.Vals, *icol.Get(c))
+		}
+		// TODO: skip this if the index contains all the columns
+		ok, err := dbGet(sc.db, tdef, rec)
+		BTree.Assert(ok && err == nil, "error encoutered while dereferencing the current row by secondary index")
+	}
+}
+
+func (db *DB) Scan(table string, req *Scanner) error {
+	tdef := getTableDef(db, table)
+	if tdef == nil {
+		return fmt.Errorf("table not found: %s", table)
+	}
+	return dbScan(db, tdef, req)
+}
+
+func dbScan(db *DB, tdef *TableDef, req *Scanner) error {
+	// sanity checks
+	switch {
+	case req.Cmp1 > 0 && req.Cmp2 < 0:
+	case req.Cmp2 > 0 && req.Cmp1 < 0:
+	default:
+		return fmt.Errorf("bad range")
+	}
+
+	// select an index
+	indexNo, err := findIndex(tdef, req.Key1.Cols)
+	if err != nil {
+		return err
+	}
+	index, prefix := tdef.Cols[:tdef.PKeys], tdef.Prefix
+	if indexNo >= 0 {
+		index, prefix = tdef.Indexes[indexNo], tdef.IndexPrefixes[indexNo]
+	}
+	req.db = db
+	req.tdef = tdef
+	req.indexNo = indexNo
+
+	// seek to the start key
+	keyStart := encodeKeyPartial(
+		nil, prefix, req.Key1.Vals, tdef, index, req.Cmp1)
+	req.keyEnd = encodeKeyPartial(
+		nil, prefix, req.Key2.Vals, tdef, index, req.Cmp2)
+	req.iter = db.kv.tree.Seek(keyStart, req.Cmp1)
+	return nil
+}
+
+// within the range or not?
+func (sc *Scanner) Valid() bool {
+	if !sc.iter.Valid() {
+		return false
+	}
+	key, _ := sc.iter.Deref()
+	return cmpOK(key, sc.Cmp2, sc.keyEnd)
+}
+
+// move the underlying B-tree iterator
+func (sc *Scanner) Next() {
+	BTree.Assert(sc.Valid(), "scanner is not valid")
+	if sc.Cmp1 > 0 {
+		sc.iter.Next()
+	} else {
+		sc.iter.Prev()
+	}
+}
+
+// get a single row by the primary key
+func dbGet(db *DB, tdef *TableDef, rec *Record) (bool, error) {
+	// just a shortcut for the scan operation
+	sc := Scanner{
+		Cmp1: CMP_GE,
+		Cmp2: CMP_LE,
+		Key1: *rec,
+		Key2: *rec,
+	}
+
+	if err := dbScan(db, tdef, &sc); err != nil {
+		return false, err
+	}
+
+	if sc.Valid() {
+		sc.Deref(rec)
+		return true, nil
+	} else {
+		return false, nil
+	}
+}
