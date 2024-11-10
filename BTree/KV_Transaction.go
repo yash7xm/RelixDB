349cc8e6b3547d38992bfbf53bd9b0aef0338bd2
yash7xm <yash.7xm@gmail.com>
2024-11-10 13:06:32 +0530

    transaction roolback added

--- BTree/KV_Transaction.go
+++ BTree/KV_Transaction.go
@@ -18,3 +18,18 @@ func (kv *KV) Begin(tx *KVTX) {
 	tx.tree.root = kv.tree.root
 	tx.free.head = kv.free.head
 }
+
+// roolback the tree and other in-memory data structures.
+func roolbackTX(tx *KVTX) {
+	kv := tx.db
+	kv.tree.root = tx.tree.root
+	kv.free.head = tx.free.head
+	kv.page.nfree = 0
+	kv.page.nappend = 0
+	kv.page.updates = map[uint64][]byte{}
+}
+
+// end a transaction: roolback
+func (kv *KV) Abort(tx *KVTX) {
+	roolbackTX(tx)
+}
