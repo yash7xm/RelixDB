d67861b88deae547ead2cfe91ce7eba2302c58bb
yash7xm <yash.7xm@gmail.com>
2024-11-16 16:20:47 +0530

    FIX: initdb()

--- app/table_init.go
+++ app/table_init.go
@@ -1,24 +1,12 @@
 package relixdb
 
 import (
-	"fmt"
 	"log"
 )
 
 const PATH = "../archive/testdb"
 
-func InitDB() {
-	db := &DB{
-		Path:   PATH,
-		kv:     KV{Path: PATH},
-		tables: make(map[string]*TableDef),
-	}
-
-	if err := db.kv.Open(); err != nil {
-		fmt.Printf("KV.Open() failed: %v", err)
-	}
-	defer db.kv.Close()
-
+func InitDB(db *DB) {
 	err := db.TableNew(TDEF_TABLE)
 	if err != nil {
 		log.Fatalf("failed to create @table table: %v", err)
